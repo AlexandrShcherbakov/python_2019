1cc8520a9a152e1b582066bdaebe5e1896371df1
AlexandrShcherbakov <alex.shcherbakov.as@gmail.com>
2019-03-11 21:12:46 +0300

    Add increase button

--- 1/main.py
+++ 1/main.py
@@ -3,6 +3,14 @@ import tkinter
 
 root = tkinter.Tk()
 button = tkinter.Button(root, text="Button 1", command=root.quit)
+
+increase_button = tkinter.Button(root, text="0")
+def inc_func(*args):
+	increase_button["text"] = str(int(increase_button["text"]) + 1)
+increase_button.bind("<Button-1>", inc_func)
+
 button.grid()
+increase_button.grid()
+
 root.mainloop()
 root.destroy()
