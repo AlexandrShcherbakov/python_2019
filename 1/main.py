a3766e13c6d549e39f80bfbcd4e12727a2dd6b64
AlexandrShcherbakov <alex.shcherbakov.as@gmail.com>
2019-03-11 21:20:03 +0300

    Add set title button

--- 1/main.py
+++ 1/main.py
@@ -1,4 +1,12 @@
+import random
 import tkinter
+
+
+titles = [
+	"Hello, world!",
+	"I'll be back",
+	"Segmentation fault",
+]
 
 
 root = tkinter.Tk()
@@ -9,8 +17,14 @@ def inc_func(*args):
 	increase_button["text"] = str(int(increase_button["text"]) + 1)
 increase_button.bind("<Button-1>", inc_func)
 
+def set_title_func():
+	root.title(random.choice(titles))
+
+set_title_button = tkinter.Button(root, text="Change title", command=set_title_func)
+
 button.grid()
 increase_button.grid()
+set_title_button.grid()
 
 root.mainloop()
 root.destroy()
